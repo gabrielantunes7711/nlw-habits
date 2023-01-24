3a97d5f458fce1c1a3d018650499d58994e8c61e
Gabriel Antunes <gabriel@wp2b.com.br>
2023-01-24 20:40:58 -0300

    Put weekday

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,15 @@
 const form = document.querySelector("form");
 const nlwSetup = new NLWSetup(form);
+const weekdayNames = [
+  "Segunda",
+  "Terça",
+  "Quarta",
+  "Quinta",
+  "Sexta",
+  "Sábado",
+  "Domingo",
+];
+const currentWeekday = weekdayNames[new Date().getDay()];
 
 const button = document.querySelector("header button");
 
@@ -17,14 +27,29 @@ function add() {
 
   nlwSetup.addDay(today);
   alert("Adicionado com sucesso");
+
+  putWeekday();
 }
 
 function save() {
   localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data));
 }
 
+function putWeekday() {
+  const allAddedDay = document.querySelectorAll(".day div");
+
+  console.log(allAddedDay);
+
+  if (allAddedDay != []) {
+    for (const addedDay of allAddedDay) {
+      addedDay.innerHTML += `<span>${currentWeekday}</span>`;
+    }
+  }
+}
 const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {};
 
 nlwSetup.setData(data);
 
 nlwSetup.load();
+
+putWeekday();
